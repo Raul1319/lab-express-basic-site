9bac5f8cb026911c9c9b02c759042f49b0b3446b
johndoe@example.com <johndoe@example.com>
2022-10-12 16:54:29 +0100

    done

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use( express.static("public"))
+app.use( express.static("public"));
+
+app.get("/", (req, res) =>{
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (req, res) =>{
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (req, res) =>{
+  res.sendFile(__dirname + "views/works.html")
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
